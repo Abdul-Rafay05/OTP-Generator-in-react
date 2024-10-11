35fa535541773fc49beded63abca03b49d6d34b7
Abdul-Rafay05 <syedabdulrafay163@gmail.com>
2024-10-12 01:26:36 +0500

    changes

--- src/Component/OTP.jsx
+++ src/Component/OTP.jsx
@@ -5,9 +5,9 @@ function Otp(props) {
   const [OTP, setOtp] = useState("00000");
   return (
     <>
-      <div className="container flex flex-col justify-center items-center w-full h-screen gap-5 bg-gradient-to-r from-[#02ff02] to-[#003780]">
+      <div className="container flex flex-col justify-center items-center w-full h-screen gap-5 bg-gradient-to-r from-[#7E60BF] to-[#E4B1F0] text-white">
         <h1 className="uppercase text-3xl font-bold">{props.mainHead}</h1>
-        <div className="otp_container text-5xl border-solid border-2 border-black tracking-[5px] font-bold px-10 py-5 rounded-lg">
+        <div className="otp_container text-5xl border-solid border-2 border-black tracking-[5px] font-bold px-10 py-5 rounded-lg text-white">
           {OTP}
         </div>
         <button
