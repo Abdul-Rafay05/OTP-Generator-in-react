f0de94f9aac7e3bdf425ee520906f408cd180e23
Abdul-Rafay05 <syedabdulrafay163@gmail.com>
2024-10-14 03:56:12 +0500

    addcustompasswordgenerator

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import OTP from "./Component/OTP.jsx";
+import PasswordGenerator from "./Component/passwordgenerator.jsx";
+import CustomPassGenerator from "./Component/custompassgenerator.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <OTP mainHead="otp generator"/>
+    {/* <OTP mainHead="otp generator" /> */}
+    {/* <PasswordGenerator /> */}
+    <CustomPassGenerator />
   </StrictMode>
 );
